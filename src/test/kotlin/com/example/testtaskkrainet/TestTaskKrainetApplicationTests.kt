ae3e1d2b2019d13a4f4c99700bdb1f42dafcbbe8
rand-06 <pogtamaxim10@gmail.com>
2025-03-12 16:16:03 +0300

    first commit :saluting_face:

---
+++ src/test/kotlin/com/example/testtaskkrainet/TestTaskKrainetApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.testtaskkrainet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestTaskKrainetApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
